b6c4365ac4574590a96c7fc365eb0e32e053cef9
Brian Segers <briansegs@gmail.com>
2020-09-07 13:37:57 -0400

    Change Tests *Refactored password_generator and tests needed "_rf" to work

--- Strings_Cheatsheet.py
+++ Strings_Cheatsheet.py
@@ -21,20 +21,21 @@ def username_generator_rf(first_name, last_name):
     username = first_name[:3] + last_name[:4]
   return username
 #Note: turns out that Python handels slicing "gracefully" so this code was unnessary and my first attempt works better. No out of bounds errors.
-
+#Test
 print(username_generator('Ned','Red'))
 print(username_generator('Shroom','Bloom'))
 
 print(username_generator_rf('Al','Q'))
 print(username_generator_rf('Abe','Simpson'))
 
-def password_generator(username):
+def password_generator_rf(username):
   password = ""
   for index in range(len(username)):
     password += username[index - 1] 
   return password
+#Test
+print(password_generator_rf('NedRed'))
+print(password_generator_rf('ShrBloo'))
+print(password_generator_rf('AlQ'))
+print(password_generator_rf('AbeSimp'))
 
-print(password_generator('NedRed'))
-print(password_generator('ShrBloo'))
-print(password_generator('AlQ'))
-print(password_generator('AbeSimp'))
