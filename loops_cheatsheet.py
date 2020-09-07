db48420349f3560615829959a9c888cc14d164e3
Brian Segers <briansegs@gmail.com>
2020-09-07 13:17:11 -0400

    Add Divisible by ten

--- loops_cheatsheet.py
+++ loops_cheatsheet.py
@@ -2,3 +2,14 @@
 #for <temporary variable> in <list variable>:
 #  <action statement>
 #  <action statement>
+
+#Counts items in list and checks if they can be divided by 10
+def divisible_by_ten(nums):
+  count = 0
+  for i in nums:
+    if i % 10 == 0:
+      count += 1
+  return count    
+
+print(divisible_by_ten([20, 25, 30, 35, 40]))
+
