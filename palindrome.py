f76e4d68826cb06bf024f05e09bc08f71ecdcea6
Brian Segers <briansegs@gmail.com>
2020-09-28 13:16:53 -0400

    Add Palindrome refactored with recursive algorithm

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,4 @@
+#Given the string, check if it is a palindrome.
 def checkPalindrome(word):
     check = -1
     for letter in word:
@@ -7,5 +8,17 @@ def checkPalindrome(word):
             return False
     return True    
     
-    
 print(checkPalindrome('aabaa'))    
+
+
+#Palindrome refactored with recursive algorithm.
+
+def checkPalindrome_r(word):
+    if len(word) <= 1:
+        return True
+    if word[0] != word[-1]:
+        return False
+    else:
+        return checkPalindrome_r(word[1: -1])
+
+print(checkPalindrome_r('aabaa')) 
